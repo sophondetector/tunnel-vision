0ae6ed5e1d156af1b62673ea809b52e112ef9c6c
sophondetector <nateisgood@gmail.com>
2026-03-02 17:37:04 -0500

    render using canvas

--- src/pdf-reader.ts
+++ src/pdf-reader.ts
@@ -4,18 +4,35 @@ import pdfWorkerUrl from 'pdfjs-dist/build/pdf.worker.min.mjs?url'
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorkerUrl;
 
-var text = ''
 var loadingTask = pdfjsLib.getDocument('../test-pdf.pdf')
 
-loadingTask.promise.then((pdf) => pdf.getPage(1))
-  .then((page) => page.getTextContent())
-  .then((tc) => {
-    for (const ite of tc.items) {
-      //@ts-ignore
-      text += ite.str + '\n'
-    }
-  })
-  .then(() => {
-    document.querySelector('#readout')!.textContent = text
-  })
+loadingTask.promise.then(function (pdf) {
+  return pdf.getPage(1)
+})
+  .then(function (page) {
+    var scale = 2;
+    var viewport = page.getViewport({ scale: scale, });
+    // Support HiDPI-screens.
+    var outputScale = window.devicePixelRatio || 1;
 
+    var canvas: HTMLCanvasElement = document.getElementById('the-canvas') as HTMLCanvasElement;
+    var context = canvas.getContext('2d');
+
+    canvas.width = Math.floor(viewport.width * outputScale);
+    canvas.height = Math.floor(viewport.height * outputScale);
+    canvas.style.width = Math.floor(viewport.width) + "px";
+    canvas.style.height = Math.floor(viewport.height) + "px";
+
+    var transform = outputScale !== 1
+      ? [outputScale, 0, 0, outputScale, 0, 0]
+      : null;
+
+    var renderContext = {
+      canvasContext: context,
+      transform: transform,
+      viewport: viewport
+    };
+    //@ts-ignore
+    page.render(renderContext);
+  })
+  .then(() => console.log('done'))
