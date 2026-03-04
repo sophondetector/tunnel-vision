d793d3a4d2d7ba715bc4c74a1f979b9e4fccb5c9
sophondetector <nateisgood@gmail.com>
2026-03-03 22:04:01 -0500

    new version of pdfReaderElementGetter that gets all spans in multicolumn layouts but breaks in the middle of lines

--- src/tunnel-vision/site-handlers/pdf-reader-handler.ts
+++ src/tunnel-vision/site-handlers/pdf-reader-handler.ts
@@ -5,9 +5,17 @@ export function isPdfReader(): boolean {
 }
 
 function pdfReaderElementGetter(): Array<Element> | null {
-  const textLayer = document.getElementById('text-layer') as HTMLDivElement
-  return [textLayer]
+  return Array.from(document.querySelectorAll('#text-layer span'))
 }
+
+// FIXME: the above version captures all the text even in the two columns
+// but the below version does a better job of not breaking in the middle of 
+// lines - reconcile the two
+
+// function pdfReaderElementGetter(): Array<Element> | null {
+//   const textLayer = document.getElementById('text-layer') as HTMLDivElement
+//   return [textLayer]
+// }
 
 export const pdfReaderHandler: TvHandler = {
   getTvElements: pdfReaderElementGetter,
