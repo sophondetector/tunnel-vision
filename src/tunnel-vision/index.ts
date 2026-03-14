0c26ed4b78fd631c4391979aa2ef4005616d3823
sophondetector <nateisgood@gmail.com>
2026-03-14 18:24:35 -0400

    add TODO and FIXME

--- src/tunnel-vision/index.ts
+++ src/tunnel-vision/index.ts
@@ -43,8 +43,8 @@ export class TvDirector {
   }
 
   setOnNav(): void {
-    //TODO this causes multiple runs of init when new pages load
-    //TODO replace this with a "milliseconds since last tree manipulation" debounce
+    //FIXME: this causes multiple runs of init when new pages load
+    //TODO: replace this with a "milliseconds since last tree manipulation" debounce
     //@ts-ignore
     window.navigation.onnavigatesuccess = () => {
       clearTimeout(NAV_DEBOUNCE)
