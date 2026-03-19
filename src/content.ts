19a62d8745bf81387b1c6920445f5a4d583195ad
sophondetector <nateisgood@gmail.com>
2026-03-19 16:06:25 -0400

    get rid of init-delay TODO

--- src/content.ts
+++ src/content.ts
@@ -2,19 +2,3 @@ import { initializeTV } from "./initialize-tv";
 
 initializeTV()
 
-// TODO: integrate the below pattern into the initializeTV function
-// so the elementGetter doesn't fire until some condition is met
-
-// NOTE: THIS IS A "ROUGH AND READY" SOLUTION FOR SITES WHERE THE 
-// ELEMENT GETTER IS FIRING BEFORE THE SITE IS LOADED
-
-// const WAIT = 3000
-//
-// setTimeout(
-//   () => {
-//     console.log(`initing after ${WAIT} milliseconds`)
-//     initializeTV()
-//   },
-//   WAIT
-// )
-//
