34ea58a4ab4a9b98b98257b77d8cfac5936de150
sophondetector <nateisgood@gmail.com>
2026-03-25 15:27:27 -0400

    add click.mp3 and actual sound playing to playSound() and add click.mp3 to web_accessible_resources in manifest.json

--- src/tunnel-vision/sound.ts
+++ src/tunnel-vision/sound.ts
@@ -1,3 +1,14 @@
+const AUDIO_URL = 'src/tunnel-vision/click.mp3'
+
+let AUDIO: HTMLAudioElement | null = null
+
+function getAudio(): HTMLAudioElement {
+  if (AUDIO === null) {
+    AUDIO = new Audio(chrome.runtime.getURL(AUDIO_URL));
+  }
+  return AUDIO
+}
+
 export function playSound(): void {
-  console.log('SOUND PLAYS HERE')
+  getAudio().play();
 }
