4b3e25fa63f6409ab2b18b187d0414853d2c7ffe
sophondetector <nateisgood@gmail.com>
2026-03-26 11:40:58 -0400

    change global vars in options.ts to capital case

--- src/options.ts
+++ src/options.ts
@@ -8,14 +8,13 @@ import {
   GET_STATE
 } from "./common";
 
-// TODO: change all these to capital case
-const toggle = document.getElementById('tv-toggle') as HTMLButtonElement
-const slider = document.getElementById('opacity-slider') as HTMLInputElement
-const opacityDisplay = document.getElementById('opacity-display') as HTMLInputElement
-const colorPicker = document.getElementById('color-picker') as HTMLInputElement
-const pdfToggle = document.getElementById('pdf-toggle') as HTMLButtonElement
-const soundToggle = document.getElementById('sound-toggle') as HTMLButtonElement
-const soundDisplay = document.getElementById('sound-display') as HTMLSpanElement
+const TOGGLE = document.getElementById('tv-toggle') as HTMLButtonElement
+const OPACITY_SLIDER = document.getElementById('opacity-slider') as HTMLInputElement
+const OPACITY_DISPLAY = document.getElementById('opacity-display') as HTMLInputElement
+const COLOR_PICKER = document.getElementById('color-picker') as HTMLInputElement
+const PDF_TOGGLE = document.getElementById('pdf-toggle') as HTMLButtonElement
+const SOUND_TOGGLE = document.getElementById('sound-toggle') as HTMLButtonElement
+const SOUND_DISPLAY = document.getElementById('sound-display') as HTMLSpanElement
 
 function urlIsPdf(url: string): boolean {
   if (url.match(/\.pdf$/)) return true
@@ -24,36 +23,36 @@ function urlIsPdf(url: string): boolean {
 
 function displaySoundState(soundOn: boolean): void {
   if (soundOn) {
-    soundDisplay.textContent = 'Sound is On'
+    SOUND_DISPLAY.textContent = 'Sound is On'
     return
   }
-  soundDisplay.textContent = 'Sound is Off'
+  SOUND_DISPLAY.textContent = 'Sound is Off'
 }
 
-toggle.addEventListener('click', async () => {
+TOGGLE.addEventListener('click', async () => {
   const tab = await getCurrentTab()
   chrome.tabs.sendMessage(tab.id!, TOGGLE_SCREEN, function () {
     console.log("sent message to content.ts in open tab")
   })
 })
 
-soundToggle.addEventListener('click', () => {
+SOUND_TOGGLE.addEventListener('click', () => {
   toggleSound()
     .then(soundIsOn)
     .then(displaySoundState)
 })
 
-slider.addEventListener("input", async (event) => {
+OPACITY_SLIDER.addEventListener("input", async (event) => {
   //@ts-ignore
   const value = event.target.value
   const tab = await getCurrentTab()
   chrome.tabs.sendMessage(tab.id!, `${value}`, function () {
-    opacityDisplay.textContent = `${value}%`
+    OPACITY_DISPLAY.textContent = `${value}%`
     console.log(`sent screen opacity value ${value} to content.ts in open tab`)
   })
 })
 
-colorPicker.addEventListener("input", async (event) => {
+COLOR_PICKER.addEventListener("input", async (event) => {
   //@ts-ignore
   const value = event.target.value
   const tab = await getCurrentTab()
@@ -66,17 +65,17 @@ getCurrentTab()
   .then(tab => {
 
     if (tab.url && urlIsPdf(tab.url)) {
-      pdfToggle.style.backgroundColor = "lightgreen"
-      pdfToggle.addEventListener("click", async () => {
+      PDF_TOGGLE.style.backgroundColor = "lightgreen"
+      PDF_TOGGLE.addEventListener("click", async () => {
         await storeLatestPDFUrlInLocalStorage(tab.url as string)
       })
     }
 
     chrome.tabs.sendMessage(tab.id!, GET_STATE, function (state: TvScreenState) {
       const opacityPercent = String(state.opacity * 100)
-      slider.value = opacityPercent
-      opacityDisplay.textContent = `${opacityPercent}%`
-      colorPicker.value = state.hexColor
+      OPACITY_SLIDER.value = opacityPercent
+      OPACITY_DISPLAY.textContent = `${opacityPercent}%`
+      COLOR_PICKER.value = state.hexColor
     })
 
   })
