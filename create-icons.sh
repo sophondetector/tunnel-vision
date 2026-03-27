c4be3f22f63ac6f1075a6a5218666c9601db3207
sophondetector <nateisgood@gmail.com>
2026-03-26 20:27:37 -0400

    modify create-images.sh to create red versions of the icons

--- create-icons.sh
+++ create-icons.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+
+# # Strong red tint
+# magick input.png -fill red -colorize 50 output-red.png
+#
+# # Strong green tint
+# magick input.png -fill green -colorize 50 output-green.png
+
 BASE_IMAGE="logo/Base-Logo.png"
 OUTPUT_DIR="public/images"
 
@@ -9,9 +16,28 @@ function create_image() {
   printf "%s created\n" "$output_name"
 }
 
+function create_image_red() {
+  output_name=$OUTPUT_DIR/red/icon-$1.png
+  magick $BASE_IMAGE -fill red -colorize 50 -resize "$1x$1" "$output_name"
+  printf "%s created\n" "$output_name"
+}
+
+printf "creating base icons..."
+
 create_image 128
 create_image 48
 create_image 32
 create_image 16
+
+printf "done\n"
+
+printf "creating red icons..."
+
+create_image_red 128
+create_image_red 48
+create_image_red 32
+create_image_red 16
+
+printf "done\n"
 
 printf "Icon creation done.\n"
