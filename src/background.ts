8bf6d82470fb0b3b490f5dc2d25122cd550693b2
sophondetector <nateisgood@gmail.com>
2026-03-27 09:57:09 -0400

    placeholder code which changes icon based on whether page loads at an even or odd numbered Date.time()

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,42 @@
 import { setSoundOn } from "./common"
 
 chrome.runtime.onInstalled.addListener(setSoundOn)
+
+function setIconRed() {
+  chrome.action.setIcon({
+    path: {
+      "16": "images/red/icon-16.png",
+      "32": "images/red/icon-32.png",
+      "48": "images/red/icon-48.png",
+      "128": "images/red/icon-128.png"
+    }
+  });
+}
+
+function setIconDefault() {
+  chrome.action.setIcon({
+    path: {
+      "16": "images/icon-16.png",
+      "32": "images/icon-32.png",
+      "48": "images/icon-48.png",
+      "128": "images/icon-128.png"
+    }
+  });
+}
+
+// every time active tab changes
+// ask the active tab for the director state
+// change icon to reflect that state
+chrome.runtime.onMessage.addListener(
+  (msg, sender, sendResponse) => {
+    if (msg === "RED") {
+      setIconRed()
+    } else if (msg === "DEFAULT") {
+      setIconDefault()
+    } else {
+      console.log(msg)
+      console.log(sender)
+    }
+    sendResponse()
+  }
+)
--- src/content.ts
+++ src/content.ts
@@ -2,3 +2,7 @@ import { initializeTV } from "./initialize-tv";
 
 initializeTV()
 
+const STATE = (new Date().getTime() % 2 === 0) ? "RED" : "DEFAULT"
+
+chrome.runtime.sendMessage(STATE)
+
