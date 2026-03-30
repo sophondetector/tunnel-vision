679240372c4ce209374102d47aff0c0d4d95b1c6
sophondetector <nateisgood@gmail.com>
2026-03-30 16:26:24 -0400

    remove TODO

--- src/pdf-reader.ts
+++ src/pdf-reader.ts
@@ -5,7 +5,6 @@ import { initializeTV, getDirector } from './initialize-tv';
 import { getCurrentTab, LATEST_PDF_URL_KEY, storePDFUrlInLocalStorage } from './common';
 import { Z_INDEX } from './tunnel-vision/tv-screen';
 
-// TODO: make icon change color when (1) unavailable (2) ranged successfully or (3) tried to range but error
 // FIXME: auto scrolling the range into view doesn't work in the pdf viewer
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorkerUrl;
