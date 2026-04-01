43a4dd4be2bb0b24cddbb1283821cefa26dfa42c
sophondetector <nateisgood@gmail.com>
2026-04-01 10:47:16 -0400

    tailwind css now works with vite dev server; added a bunch of explicit directives to vite.config.js; added tailwind.config.js; upgraded crxjs/vite-plugin from 2.0.0 to 2.4.0 (this seems to be really what fixed it)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/options.html",
+    "./pages/pdf-reader.html",
+    "./src/**/*.{js,ts,jsx,tsx,html,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
