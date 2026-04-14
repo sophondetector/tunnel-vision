877b5c22b18abd9ab7040db1acaf8e9deeb25ae7
sophondetector <nateisgood@gmail.com>
2026-04-13 21:07:42 -0400

    make vatican-handler getTvElements generic because its useful for debugging

--- src/tunnel-vision/site-handlers/vatican-handler.ts
+++ src/tunnel-vision/site-handlers/vatican-handler.ts
@@ -1,16 +1,18 @@
 import { blankDelay, TvHandler } from "./handler-utilities"
 
-async function vaticanElementGetter(): Promise<Element[] | null> {
-  const mainContent = document.querySelector('.documento')
-  if (!mainContent) {
-    console.log(`vaticanElementGetter: could not find mainContent`)
-    return null
-  }
-  return [mainContent]
-}
+// TODO: maybe switch this back on or maybe just get rid of site handlers entirely - idk
+
+// async function vaticanElementGetter(): Promise<Element[] | null> {
+//   const mainContent = document.querySelector('.documento')
+//   if (!mainContent) {
+//     console.log(`vaticanElementGetter: could not find mainContent`)
+//     return null
+//   }
+//   return [mainContent]
+// }
 
 export const vaticanHandler: TvHandler = {
-  getTvElements: vaticanElementGetter,
+  getTvElements: async () => [document.body],
   getScrollableElement: async () => null,
   initDelay: blankDelay,
   mutationHandler: null
