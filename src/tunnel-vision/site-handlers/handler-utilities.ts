2ff736b967516f18d27d9b214dd898f45cd7b0cd
sophondetector <nateisgood@gmail.com>
2026-04-24 10:29:59 -0400

    get rid of FIXME

--- src/tunnel-vision/site-handlers/handler-utilities.ts
+++ src/tunnel-vision/site-handlers/handler-utilities.ts
@@ -80,8 +80,6 @@ export function discoverScrollableFromCenter(): Element | null {
 
 // TODO: discoverScrollable by walking the entire tree and choosing the widest scrollable element
 
-// FIXME: figure out how to make ranges only scroll the scrollable element they are actually in
-
 export async function discoverScrollableFromCenterPromise(): Promise<Element | null> {
   return new Promise((resolve) => {
     requestAnimationFrame(() => {
