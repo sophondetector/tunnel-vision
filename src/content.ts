debfb06516677a24dbce40aa1290f66c87a8e3a1
sophondetector <nateisgood@gmail.com>
2026-05-04 16:13:37 -0400

    if we are on the web app; don't activate the director but instead let the web app director handle things

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,14 @@
 import { initDirectorForChromeExtension } from "./initialize-tv";
 
-initDirectorForChromeExtension()
+// TODO: a more "bullet-proof" check for whether we're on the web app
+function weAreOnPdfWebApp(): boolean {
+  return document.querySelector('#viewerContainer') ? true : false
+}
+
+if (!weAreOnPdfWebApp()) {
+  initDirectorForChromeExtension()
+} else {
+  // TODO: inject a link to the extension viewer "its better in the extension viewer"
+  console.log("using web app director - skipping extension director")
+}
+
