0d5447ae50b709dfda842199cd77a958c68d13ea
sophondetector <nateisgood@gmail.com>
2026-05-06 15:31:28 -0400

    make app check better

--- src/content.ts
+++ src/content.ts
@@ -1,14 +1,13 @@
 import { initDirectorForChromeExtension } from "./initialize-tv";
 
-// TODO: a more "bullet-proof" check for whether we're on the web app
 function weAreOnPdfWebApp(): boolean {
-  return document.querySelector('#viewerContainer') ? true : false
+  return document.title === "Vision PDF"
 }
 
-if (!weAreOnPdfWebApp()) {
-  initDirectorForChromeExtension()
-} else {
+if (weAreOnPdfWebApp()) {
   // TODO: inject a link to the extension viewer "its better in the extension viewer"
   console.log("using web app director - skipping extension director")
+} else {
+  initDirectorForChromeExtension()
 }
 
